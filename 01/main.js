5d4a56b0d6ba3bfcb7127a93b5e44c2099633c4c
Hubert Gendron <hubert.gendron689@gmail.com>
2020-12-01 16:43:36 -0500

    fix iterator

--- 01/main.js
+++ 01/main.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
+const { mainModule } = require('process');
 const input = fs.readFileSync('./input.txt', {encoding: 'utf-8'});
 
 const values = input.split('\n').map(x => parseInt(x, 10));
 
 for (var i = 0; i < values.length; ++i) {
-    for (var j = i+1; j < values.length; ++j) {
-        if (values[i] + values[j] === 2020) {
+    for (var j = 0; j < values.length; ++j) {
+        if (i != j && values[i] + values[j] === 2020) {
             console.log(values[i] * values[j])
             break;
         }
     }
 }
+
