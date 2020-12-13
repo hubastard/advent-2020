113e6b159f911fe879692ebdb54b341814432a8d
Hubert Gendron <hubert.gendron689@gmail.com>
2020-12-12 23:59:23 -0500

    fix part 2 of day 1

--- 01/main.js
+++ 01/main.js
@@ -3,12 +3,38 @@ const input = fs.readFileSync('./input.txt', {encoding: 'utf-8'});
 
 const values = input.split('\n').map(x => parseInt(x, 10));
 
-for (var i = 0; i < values.length; ++i) {
-    for (var j = 0; j < values.length; ++j) {
-        if (i != j && values[i] + values[j] === 2020) {
-            console.log(values[i] * values[j])
-            break;
+function part1(goal) {
+    const workingSet = [];
+    for (var i = 0; i < values.length; ++i) {
+        const diff = goal - values[i];
+        if (workingSet.includes(diff)) {
+            return {
+                a: diff,
+                b: values[i],
+                result: diff * values[i]
+            };
+        } else {
+            workingSet.push(values[i]);
+        }
+    }
+
+    return undefined;
+}
+
+function part2(goal) {
+    for (var i = 0; i < values.length; ++i) {
+        const diff = goal - values[i];
+        const result = part1(diff);
+
+        if (result !== undefined) {
+            return result.a * result.b * values[i];
         }
     }
 }
+
+console.log(part1(2020).result);
+console.log(part2(2020));
+
+
+
 
